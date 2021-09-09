fd97bfd8e979fbc3a8dcdd782f3886cf4f4f69e1
bbbnam <bbbnam@naver.com>
2021-09-10 00:15:22 +0900

    feat : HelloResponseDto 생성

---
+++ src/main/java/com/jdrake/book/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package com.jdrake.book.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+}
