84dd559a4b82b751b8bca0c1a0a971af93d44e3d
bbbnam <bbbnam@naver.com>
2021-09-09 23:53:41 +0900

    feat : 주석작성

--- src/main/java/com/jdrake/book/springboot/Application.java
+++ src/main/java/com/jdrake/book/springboot/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        // 내장 Was 실행
         SpringApplication.run(Application.class, args);
     }
 }
