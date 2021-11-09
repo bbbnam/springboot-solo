ded313f7c58ab9aca24fc27521bc6f61a1811057
bbbnam <bbbnam@naver.com>
2021-11-09 21:45:42 +0900

    refactor : format change

--- src/main/java/com/jdrake/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jdrake/book/springboot/service/posts/PostsService.java
@@ -23,13 +23,13 @@ public class PostsService {
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
         posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
     }
 
     public PostsResponseDto findById(Long id) {
         return new PostsResponseDto(postsRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id =" + id)));
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id)));
     }
 }
